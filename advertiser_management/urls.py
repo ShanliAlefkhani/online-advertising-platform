64f4aa2b6930e4c8cd7bc6caa2af36916b47ad80
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-04-03 18:31:51 +0330

    Add ad_create_view. Add ad_create.html Add url.

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from advertiser_management import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('click/<int:object_id>/', views.detail, name='detail'),
+    path('index', views.index_view, name='index'),
+    path('click/<int:object_id>/', views.detail_view, name='detail'),
+    path('create_ad', views.ad_create_view, name='ad_create')
 ]
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from advertiser_management.models import Advertiser, Ad
 from django.shortcuts import redirect
+from .forms import AdForm
 
 
-def index(request):
+def index_view(request):
     advertisers = Advertiser.objects.all()
     for advertiser in advertisers:
         for ad in advertiser.ad_set.all():
@@ -13,8 +14,16 @@ def index(request):
     return render(request, 'ads.html', context)
 
 
-def detail(request, object_id):
+def detail_view(request, object_id):
     ad = Ad.objects.get(id=object_id)
     ad.clicks += 1
     ad.save()
     return redirect(ad.link)
+
+
+def ad_create_view(request):
+    form = AdForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {'form': form}
+    return render(request, 'ad_create.html', context)
