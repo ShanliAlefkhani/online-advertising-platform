8b348db98f2c8827a3e39d4612ebada0233d2307
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-04-05 03:16:39 +0330

    Add Click and View models.

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -7,8 +7,18 @@ class Advertiser(models.Model):
 
 class Ad(models.Model):
     advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
-    clicks = models.PositiveIntegerField(default=0)
-    views = models.PositiveIntegerField(default=0)
     title = models.CharField(max_length=100)
     img_url = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
+
+
+class Click(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    ip = models.CharField(max_length=100)
+
+
+class View(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    ip = models.CharField(max_length=100)
