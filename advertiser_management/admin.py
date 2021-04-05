3e4af8067907b8be2a9b16d26724b25528f68598
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-04-05 04:01:48 +0330

    Add AdAdmin to filter and search in Ads.

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from advertiser_management.models import Advertiser, Ad
 
+
+class AdAdmin(admin.ModelAdmin):
+    list_filter = ('approved',)
+    search_fields = ('title',)
+
+
 admin.site.register(Advertiser)
-admin.site.register(Ad)
+admin.site.register(Ad, AdAdmin)
