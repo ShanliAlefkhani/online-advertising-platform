0e6d82c450c197ce1bd5eb49ae32509746e471e6
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-04-06 22:14:53 +0330

    Make views class based.

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from advertiser_management import views
 
 urlpatterns = [
-    path('index', views.index_view, name='index'),
-    path('click/<int:object_id>/', views.detail_view, name='detail'),
-    path('ad_create', views.ad_create_view, name='ad_create')
+    path('index', views.IndexView.as_view(), name='index'),
+    path('click/<int:object_id>/', views.DetailView.as_view(), name='detail'),
+    path('ad_create', views.AdCreateView.as_view(), name='ad_create')
 ]
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render
 from django.urls import reverse
-from advertiser_management.models import Advertiser, Ad, Click, View
+from advertiser_management.models import Advertiser, Ad, Click, View as ViewModel
 from django.shortcuts import redirect
 from .forms import AdForm
 from datetime import datetime
+from django.views.generic import View
 
 
-def index_view(request):
-    advertisers = Advertiser.objects.all()
-    for advertiser in advertisers:
-        for ad in advertiser.ad_set.all():
-            View.objects.create(ad=ad, datetime=datetime.now(), ip="")
-    context = {'advertisers': advertisers}
-    return render(request, 'ads.html', context)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        advertisers = Advertiser.objects.all()
+        for advertiser in advertisers:
+            for ad in advertiser.ad_set.all():
+                ViewModel.objects.create(ad=ad, datetime=datetime.now(), ip="")
+        context = {'advertisers': advertisers}
+        return render(request, 'ads.html', context)
 
 
-def detail_view(request, object_id):
-    ad = Ad.objects.get(id=object_id)
-    Click.objects.create(ad=ad, datetime=datetime.now(), ip="")
-    return redirect(ad.link)
+class DetailView(View):
+    def get(self, request, object_id, *args, **kwargs):
+        ad = Ad.objects.get(id=object_id)
+        Click.objects.create(ad=ad, datetime=datetime.now(), ip="")
+        return redirect(ad.link)
 
 
-def ad_create_view(request):
-    form = AdForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    if request.method == 'GET':
+class AdCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = AdForm()
         context = {'form': form}
         return render(request, 'ad_create.html', context)
-    else:
+
+    def post(self, request, *args, **kwargs):
+        form = AdForm(request.POST or None)
+        if form.is_valid():
+            form.save()
         return redirect(reverse('index'))
