0dd7108831ff2853886e0f6c2e4069906272d474
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-04-11 15:53:52 +0330

    Basic celery task.

--- yektanet_django_project/celery.py
+++ yektanet_django_project/celery.py
@@ -6,8 +6,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yektanet_django_project.settings')
 
-app = Celery('yektanet_django_project')
+app = Celery('yektanet_django_project', broker='pyamqp://shanli:shanli@localhost')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
